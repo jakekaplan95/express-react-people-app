261293d359f8a75505c86548a0cb3d6035832b63
Jake Kaplan <kaplanjt@gmail.com>
2021-11-09 17:04:46 -0800

    commit

---
+++ src/components/Main.js
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import { Route, Switch } from "react-router-dom";
+import Index from "../pages/Index"
+import Show from "../pages/Show"
+
+function Main(props){
+    const [people, setPeople] = useState(null);
+
+    const URL = "http://localhost:4000/people/";
+
+    const getPeople = async () => {
+        const response = await fetch(URL);
+        const data = await response.json();
+        setPeople(data);
+    };
+
+    const createPeople = async (person) => {
+        // make a post request to create people
+        await fetch(URL, {
+            method: "post",
+            headers:{
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(person),
+        });
+        // update list of getPeople
+        getPeople();
+    }
+
+    const updatePeople = async (person, id) => {
+        // make a put request to update people
+        await fetch(URL + id,{
+            method: "put",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(person)
+        })
+        getPeople()
+    }
+
+    const deletePeople = async (id) => {
+        // make the delete request
+        await fetch(URL + id, {
+          method: "delete"
+        })
+        getPeople()
+      }
+
+    useEffect(() => getPeople(), []);
+
+
+    return (
+        <main>
+            <Switch>
+                <Route exact path="/">
+                    <Index people={people} createPeople={createPeople} />
+                </Route>
+                <Route path="/people/:id"
+                // using the shortcut syntax below so that we can bring our "show" without the use of return()
+                        render={(rp)=> (
+                            <Show
+                             {...rp}
+                             people={people}
+                             updatePeople={updatePeople}
+                             deletePeople={deletePeople}
+                            />
+                        )}
+                    />
+            </Switch>
+        </main>
+    )
+  }
+
+  export default Main
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles.scss';
+// Import Router
+import { BrowserRouter as Router} from "react-router-dom"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
+    <React.StrictMode>
     <App />
-  </React.StrictMode>,
+    </React.StrictMode>
+  </Router>,
   document.getElementById('root')
 );
 
